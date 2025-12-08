134dfa5702dbc926595a4ef97f118e4b78b07cf8
Nell <nadiakomuna@gmail.com>
2025-12-08 14:23:18 +0100

    dat

---
+++ db.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Restaurang (
+    restaurang_id SERIAL PRIMARY KEY,
+    name TEXT,
+    adress TEXT,
+    phone TEXT,
+    email TEXT;
+    describtion TEXT
+);
+CREATE TABLE Menu catalog(
+    
+)
